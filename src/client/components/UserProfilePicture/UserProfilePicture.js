4ae620efd2f44f0c60a8c60843e4c9f4bc1880a2
seenachacko <seenachacko119@gmail.com>
2021-07-07 22:06:09 +0200

    user profile picture component added

---
+++ src/client/components/UserProfilePicture/UserProfilePicture.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './UserProfilePicture.css';
+/* eslint-disable react/prop-types */
+function UserProfilePicture({ size, profilePictureLink }) {
+  return (
+    <img
+      className={`profile-picture ${size}`}
+      src={profilePictureLink}
+      alt="profilePicture"
+    />
+  );
+}
+
+export default UserProfilePicture;
