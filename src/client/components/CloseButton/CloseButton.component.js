d6a395ae0bd744aa7f1fa40ca8e526935632e72e
Hani-far <hfarjamy@outlook.com>
2021-07-07 12:13:11 +0200

    used className instead of class

--- src/client/components/CloseButton/CloseButton.component.js
+++ src/client/components/CloseButton/CloseButton.component.js
@@ -4,8 +4,12 @@ import './CloseButton.styles.css';
 
 const CloseButton = ({ onClick }) => {
   return (
-    <div class="icon">
-      <div class="close_button" type="button" onClick={() => onClick()}></div>
+    <div className="icon">
+      <div
+        className="close_button"
+        type="button"
+        onClick={() => onClick()}
+      ></div>
     </div>
   );
 };
