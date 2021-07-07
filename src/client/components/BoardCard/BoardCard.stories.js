7836d5d15b634519cd8fca282e2dad83454b8ff5
Islam Fawzy <islam.fawzy25@hotmail.com>
2021-07-07 10:26:12 +0200

    creat board card files

---
+++ src/client/components/BoardCard/BoardCard.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BoardCard from './BoardCard';
+
+export default {
+  title: 'Components / Board Card',
+};
+
+export const testCard = () => {
+  <BoardCard src="" width="500px" height="500px" />;
+};
