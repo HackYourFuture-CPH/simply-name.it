58bb53dffadc73c9150ad97e746f578c425bf3d2
Hani-far <hfarjamy@outlook.com>
2021-07-08 19:47:33 +0200

    changed crossButton to CloseButtonExample

--- src/client/components/CloseButton/CloseButton.stories.js
+++ src/client/components/CloseButton/CloseButton.stories.js
@@ -3,6 +3,6 @@ import CloseButton from './CloseButton.component.js';
 import { action } from '@storybook/addon-actions';
 
 export default { title: 'CloseButton' };
-export const CrossButton = () => (
+export const CloseButtonExample = () => (
   <CloseButton onClick={action('Close button clicked')} />
 );
