d91f01f7497984484f8f73672dfc1db8d68a6cf7
NatsGt <nataliarodas7858@gmail.com>
2021-07-09 11:40:11 +0200

    Removed state and managed changes with knobs

--- src/client/components/Dropdown/Dropdown.stories.js
+++ src/client/components/Dropdown/Dropdown.stories.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Dropdown from './Dropdown.component';
+import { action } from '@storybook/addon-actions';
+import { boolean } from '@storybook/addon-knobs';
 
 export default { title: 'Dropdown Menu/ Menu' };
 
 export const DropdownDark = () => {
   return (
-    <Dropdown variant="dark">
+    <Dropdown
+      variant="dark"
+      visible={boolean('visible', true)}
+      onClick={action('clicked')}
+    >
       <ul>
         <li>Option 1</li>
         <li>Option 2</li>
@@ -16,7 +22,11 @@ export const DropdownDark = () => {
 };
 export const DropdownLight = () => {
   return (
-    <Dropdown variant="light">
+    <Dropdown
+      variant="light"
+      visible={boolean('visible', true)}
+      onClick={action('clicked')}
+    >
       <ul>
         <li>Option 1</li>
         <li>Option 2</li>
