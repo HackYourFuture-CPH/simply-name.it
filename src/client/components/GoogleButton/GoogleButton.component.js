d45d6ea969fd514acd961de42d36e1de88f7debf
Wenceslao Posse Silva <wencho22@gmail.com>
2021-07-09 16:57:28 +0200

    fixed small issue after checks

--- src/client/components/GoogleButton/GoogleButton.component.js
+++ src/client/components/GoogleButton/GoogleButton.component.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './GoogleButton.css';
 
-export const GoogleButton = ({ signedIn }) => {
+export const GoogleButton = ({ hasAccount }) => {
   return (
     <div id="g-button">
       <span className="buttonText">Sign {hasAccount ? 'in' : 'up'} with</span>
