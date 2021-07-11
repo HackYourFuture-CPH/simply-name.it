150d9c4a282be7e966bd77e36581e6d9da68fc87
Islam Fawzy <islam.fawzy25@hotmail.com>
2021-07-12 00:18:35 +0200

    board card created

---
+++ src/client/components/BoardCard/BoardCard.component.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './BoardCard.style.css';
+import PropTypes from 'prop-types';
+
+export default function BoardCard({
+  src,
+  width,
+  height,
+  alt,
+  fontWeight,
+  fontSize,
+  boardTitle,
+  children,
+}) {
+  const style = {
+    width: `${width}`,
+    height: `${height}`,
+  };
+  return (
+    <div className="card-container" style={style}>
+      <img src={src} alt={alt} className="card" style={style} />
+      <div className="headerContainer">
+        <div className="title" style={{ fontSize, fontWeight }}>
+          {boardTitle}
+        </div>
+
+        {/* // D for Drop down button component  */}
+        <div className="button">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+BoardCard.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  boardTitle: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  fontWeight: PropTypes.string,
+  fontSize: PropTypes.string,
+};
+
+BoardCard.defaultProps = {
+  width: '200px',
+  height: '200px',
+  fontWeight: '300',
+  fontSize: '40px',
+};
--- src/client/components/BoardCard/BoardCard.stories.js
+++ src/client/components/BoardCard/BoardCard.stories.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import BoardCard from './BoardCard';
+import BoardCard from './BoardCard.component';
+import { text } from '@storybook/addon-knobs';
 
 export default {
   title: 'Components / Board Card',
 };
 
-export const testCard = () => {
-  <BoardCard src="" width="500px" height="500px" />;
-};
+export const testCard = () => (
+  <BoardCard
+    src={text(
+      'Image',
+      'https://www.forbes.com/health/wp-content/uploads/2021/05/baby_spit_up_1-getty_creative.jpg',
+    )}
+    width={text('Width', '400')}
+    height={text('Height', '400')}
+    boardTitle={text('Board title', 'Baby name')}
+    fontSize={text('Font size', '40px')}
+    fontWeight={text('Font weight', 'bold')}
+  />
+);
