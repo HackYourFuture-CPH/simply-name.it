3616747dcfe6b4721d0f383ec259e5babddd08e3
Islam Fawzy <islam.fawzy25@hotmail.com>
2021-07-11 22:47:27 +0200

    feedback implemented

--- src/client/components/PageTitle/PageTitle.component.js
+++ src/client/components/PageTitle/PageTitle.component.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './PageTitle.style.css';
 
 export default function PageTitle({
   title,
-  color,
   fontSize,
   fontWeight,
-  width,
+  colorVariant,
 }) {
   return (
-    // implement title in div not h1 tage  to make it more responsive with style attributes
-    <div style={{ color, fontSize, fontWeight, width }}>{title}</div>
+    <div className={` ${colorVariant}`} style={{ fontSize, fontWeight }}>
+      {title}
+    </div>
   );
 }
 
 PageTitle.propTypes = {
   title: PropTypes.string.isRequired,
-  color: PropTypes.string,
   fontSize: PropTypes.string,
   fontWeight: PropTypes.string,
-  width: PropTypes.string,
+  colorVariant: PropTypes.string,
 };
 
 PageTitle.defaultProps = {
-  color: '	#000000',
   fontSize: '100px',
   fontWeight: 'bold',
-  width: '100px',
+  colorVariant: 'black',
 };
--- src/client/components/PageTitle/PageTitle.stories.js
+++ src/client/components/PageTitle/PageTitle.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageTitle from './PageTitle.component';
+import { text } from '@storybook/addon-knobs';
 
 export default {
   title: 'Components / Page Title',
@@ -8,10 +9,8 @@ export default {
 
 export const TestTitle = () => (
   <PageTitle
-    title=" Welcome"
-    color="#ffffff"
-    fontSize="200Px"
-    fontWeight="bold"
-    width="200px"
+    title={text('Title', ' Welcome ')}
+    fontSize={text('Font-size', '60px')}
+    fontWeight={text('Font-weight', 'bold')}
   />
 );
