07a57d3a29eb494a60800589036c59c4c7ccef97
NatsGt <nataliarodas7858@gmail.com>
2021-07-11 17:29:02 +0200

    Removing files from another branch

--- src/client/components/Confetti/Confetti.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Confetti from './Confetti.component';
-
-export default { title: 'Confettianimation', component: Confetti };
-
-export const basicStory = () => <Confetti />;
