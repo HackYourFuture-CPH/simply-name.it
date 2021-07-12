44e11e912785babb01320ea211f0d239ebc0c4b2
nig <nielshtg@hotmail.com>
2021-07-12 02:33:58 +0200

    Revert "Removing files from another branch"
    This reverts commit 07a57d3a29eb494a60800589036c59c4c7ccef97.

---
+++ src/client/components/UserProfilePicture/UserProfilePicture.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import UserProfilePicture from './UserProfilePicture.component';
+
+export default {
+  title: 'Components /User Profile Picture',
+  component: UserProfilePicture,
+};
+
+export const SmallProfilePicture = () => (
+  <UserProfilePicture
+    size="small"
+    profilePictureLink="https://picsum.photos/seed/picsum/200/300"
+  />
+);
+export const BigProfilePicture = () => (
+  <UserProfilePicture
+    size="big"
+    profilePictureLink="https://picsum.photos/seed/picsum/200/300"
+  />
+);
