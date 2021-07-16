897e8b5fc73ccc3965fce60979ac3ee5b157730c
SlaviaF <slaviafurtado@gmail.com>
2021-07-16 13:55:13 +0200

    removed pic and enum, and changed uid and email

--- src/server/migrations/20210714103340_create_users_table.js
+++ src/server/migrations/20210714103340_create_users_table.js
@@ -2,11 +2,9 @@ exports.up = function (knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments();
     table.string('fullname').notNullable().index();
-    table.string('email').notNullable();
+    table.string('email').unique().notNullable().index();
     table.datetime('createdOn').defaultTo(knex.fn.now()).notNullable();
-    table.string('firebaseToken', 152).notNullable();
-    table.binary('profilePicture');
-    table.enum('role', ['creator', 'member']); // should i give a defualt here like this : .defaultTo('creator')
+    table.string('uid').notNullable();
   });
 };
 
