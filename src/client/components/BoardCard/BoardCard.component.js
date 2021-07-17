b8ead0d7eee8acd8e32afbdea04a34b5592c51c9
Islam Fawzy <islam.fawzy25@hotmail.com>
2021-07-17 13:32:05 +0200

    drag all style in css instead of using props

--- src/client/components/BoardCard/BoardCard.component.js
+++ src/client/components/BoardCard/BoardCard.component.js
@@ -2,29 +2,12 @@ import React from 'react';
 import './BoardCard.style.css';
 import PropTypes from 'prop-types';
 
-export default function BoardCard({
-  src,
-  width,
-  height,
-  alt,
-  fontWeight,
-  fontSize,
-  boardTitle,
-  children,
-}) {
-  const style = {
-    width,
-    height,
-  };
+export default function BoardCard({ src, alt, boardTitle, children }) {
   return (
-    <div className="card-container" style={style}>
-      <img src={src} alt={alt} className="card" style={style} />
+    <div className="card-container">
+      <img src={src} alt={alt} className="card" />
       <div className="headerContainer">
-        <div className="title" style={{ fontSize, fontWeight }}>
-          {boardTitle}
-        </div>
-
-        {/* // D for Drop down button component  */}
+        <div className="title">{boardTitle}</div>
         <div className="button">{children}</div>
       </div>
     </div>
@@ -36,15 +19,4 @@ BoardCard.propTypes = {
   alt: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   boardTitle: PropTypes.string.isRequired,
-  width: PropTypes.string,
-  height: PropTypes.string,
-  fontWeight: PropTypes.string,
-  fontSize: PropTypes.string,
-};
-
-BoardCard.defaultProps = {
-  width: '200px',
-  height: '200px',
-  fontWeight: '300',
-  fontSize: '40px',
 };
--- src/client/components/BoardCard/BoardCard.stories.js
+++ src/client/components/BoardCard/BoardCard.stories.js
@@ -9,27 +9,46 @@ export default {
 };
 
 export const testCard = () => (
-  <BoardCard
-    src={text(
-      'Image',
-      'https://www.forbes.com/health/wp-content/uploads/2021/05/baby_spit_up_1-getty_creative.jpg',
-    )}
-    width={text('Width', '400px')}
-    height={text('Height', '400px')}
-    boardTitle={text('Board title', 'Baby name')}
-    fontSize={text('Font size', '40px')}
-    fontWeight={text('Font weight', 'bold')}
-  >
-    <Dropdown
-      variant="dark"
-      visible={boolean('visible', true)}
-      onClick={action('clicked')}
+  <div>
+    <BoardCard
+      src={text(
+        'Image',
+        'https://www.forbes.com/health/wp-content/uploads/2021/05/baby_spit_up_1-getty_creative.jpg',
+      )}
+      alt="baby-image"
+      boardTitle={text('Board title', 'Baby name')}
     >
-      <ul>
-        <li>Option 1</li>
-        <li>Option 2</li>
-        <li>Option 3</li>
-      </ul>
-    </Dropdown>
-  </BoardCard>
+      <Dropdown
+        variant="light"
+        visible={boolean('visible', true)}
+        onClick={action('clicked')}
+      >
+        <ul>
+          <li>Option 1</li>
+          <li>Option 2</li>
+          <li>Option 3</li>
+        </ul>
+      </Dropdown>
+    </BoardCard>
+    <BoardCard
+      src={text(
+        'Image',
+        'https://www.forbes.com/health/wp-content/uploads/2021/05/baby_spit_up_1-getty_creative.jpg',
+      )}
+      alt="baby-image"
+      boardTitle={text('Board title', 'Baby name')}
+    >
+      <Dropdown
+        variant="dark"
+        visible={boolean('visible', true)}
+        onClick={action('clicked')}
+      >
+        <ul>
+          <li>Option 1</li>
+          <li>Option 2</li>
+          <li>Option 3</li>
+        </ul>
+      </Dropdown>
+    </BoardCard>
+  </div>
 );
