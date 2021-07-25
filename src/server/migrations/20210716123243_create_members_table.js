ac576c2a6bac1322abf52dce031ca9002f91a959
karokea <karo7494@kea.stud.dk>
2021-07-25 17:59:12 +0300

    unsigned and notNullable, pirmary keys, camel case columns

--- src/server/migrations/20210716123243_create_members_table.js
+++ src/server/migrations/20210716123243_create_members_table.js
@@ -1,7 +1,18 @@
 exports.up = function (knex) {
   return knex.schema.createTable('members', (table) => {
-    table.integer('board_id').references('id').inTable('boards');
-    table.integer('user_id').references('id').inTable('users');
+    table
+      .integer('boardId')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('boards');
+    table
+      .integer('userId')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users');
+    table.primary(['boardId', 'userId']);
     table.enum('role', ['member', 'admin']).defaultTo('member');
   });
 };
