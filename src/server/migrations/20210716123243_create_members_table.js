9f377bafd45e59300e18875c447a9ee8af0ee6f9
karokea <karo7494@kea.stud.dk>
2021-07-25 17:53:33 +0300

    changed ROLE column to enum

--- src/server/migrations/20210716123243_create_members_table.js
+++ src/server/migrations/20210716123243_create_members_table.js
@@ -2,10 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('members', (table) => {
     table.integer('board_id').references('id').inTable('boards');
     table.integer('user_id').references('id').inTable('users');
-    table.integer('role').references('id').inTable('roles');
-    // current DB setup does ot have roles table, but
-    // I did this in regards to Niels' comments on PR #58
-    // https://github.com/HackYourFuture-CPH/simply-name.it/pull/58
+    table.enum('role', ['member', 'admin']).defaultTo('member');
   });
 };
 
