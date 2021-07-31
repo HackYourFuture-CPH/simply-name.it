a56803e5ac58b77fe1573d21299d280736cbc9c0
karokea <karo7494@kea.stud.dk>
2021-07-31 17:15:28 +0300

    users.router

---
+++ src/server/api/routes/users.router.js
@@ -0,0 +1,34 @@
+/* TODO: This is just an example file to illustrate API routing and
+documentation. Can be deleted when the first real route is added. */
+
+const express = require('express');
+
+const router = express.Router({ mergeParams: true });
+
+// controllers
+const usersController = require('../controllers/users.controller');
+
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *    tags:
+ *    - Users
+ *    summary: Get all users
+ *    description:
+ *      Will return all users registered in the app.
+ *    produces: application/json
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.get('/', (req, res, next) => {
+  usersController
+    .getUsers()
+    .then((result) => res.json(result))
+    .catch(next);
+});
+
+module.exports = router;
