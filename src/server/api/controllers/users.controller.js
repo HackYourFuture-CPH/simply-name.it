9cfc935c17d441235f4a4c77fb6f21786b35c8fb
karokea <karo7494@kea.stud.dk>
2021-07-31 17:11:57 +0300

    users.controller with getUsers()

---
+++ src/server/api/controllers/users.controller.js
@@ -0,0 +1,18 @@
+/* TODO: This is an example controller to illustrate a server side controller.
+Can be deleted as soon as the first real controller is added. */
+
+const knex = require('../../config/db');
+const HttpError = require('../lib/utils/http-error');
+
+const getUsers = async () => {
+  return knex('users').select(
+    'users.id',
+    'users.fullname',
+    'users.createdOn',
+    'users.firebaseUId',
+  );
+};
+
+module.exports = {
+  getUsers,
+};
