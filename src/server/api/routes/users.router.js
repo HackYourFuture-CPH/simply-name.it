cd92158b7af8146a69b89bfe2b914285c8898de2
karokea <karo7494@kea.stud.dk>
2021-07-31 18:54:01 +0300

    users.router with /:memberId/boards

---
+++ src/server/api/routes/users.router.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const router = express.Router({ mergeParams: true });
+
+// controllers
+const usersController = require('../controllers/users.controller');
+
+/**
+ * @swagger
+ * /users/{ID}/boards/member:
+ *  get:
+ *    tags:
+ *    - Boards
+ *    summary: Get boards by member ID
+ *    description:
+ *      Will return the boards in which the member ID matches with the param user ID.
+ *    produces: application/json
+ *    parameters:
+ *     - in: path
+ *       name: ID
+ *       schema:
+ *         type: integer
+ *         required: true
+ *         description: The ID of the user to get
+ *
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.get('/:memberId/boards', (req, res, next) => {
+  usersController
+    .getBoardsByMemberId(req.params.memberId)
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch(next);
+});
+
+module.exports = router;
