11dcf77f233140d147024a4ac7da7784de9775b4
Moritz E. Beber <midnighter@posteo.net>
2021-08-01 15:07:47 +0200

    fix: we don't use the isBlocked field any longer
    Candidates are simply deleted at the moment. We want to keep the field
    on the database table, though.

--- src/server/seeds/development/04_candidates.js
+++ src/server/seeds/development/04_candidates.js
@@ -23,12 +23,6 @@ exports.seed = function (knex) {
           name: 'Balthromaw',
           isBlocked: false,
         },
-        {
-          id: 4,
-          boardId: 1,
-          name: 'Cutie Pie',
-          isBlocked: true,
-        },
       ]);
     });
 };
