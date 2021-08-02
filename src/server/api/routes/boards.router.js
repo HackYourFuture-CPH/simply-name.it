451fdda14fc897a53da647e191ab312c01d21434
karokea <karo7494@kea.stud.dk>
2021-08-02 17:18:17 +0300

    modified routers as per review suggestions

--- src/server/api/routes/boards.router.js
+++ src/server/api/routes/boards.router.js
@@ -3,5 +3,39 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 // controllers
+const boardsController = require('../controllers/boards.controller');
+
+/**
+ * @swagger
+ * /users/{ID}/boards/member:
+ *  get:
+ *    tags:
+ *    - Users
+ *    summary: Get boards by member ID
+ *    description:
+ *      Will return the boards in which the member ID matches with the param user ID.
+ *    produces: application/json
+ *    parameters:
+ *     - in: path
+ *       name: ID
+ *       schema:
+ *         type: integer
+ *         required: true
+ *         description: The ID of the user to get
+ *
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.get('/:memberId/boards/member', (req, res, next) => {
+  boardsController
+    .getBoardsByMemberId(req.params.memberId)
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch(next);
+});
 
 module.exports = router;
--- src/server/api/routes/users.router.js
+++ src/server/api/routes/users.router.js
@@ -1,41 +1,8 @@
 const express = require('express');
 
 const router = express.Router({ mergeParams: true });
+const boardsRouter = require('./boards.router');
 
-// controllers
-const boardsController = require('../controllers/boards.controller');
-
-/**
- * @swagger
- * /users/{ID}/boards/member:
- *  get:
- *    tags:
- *    - Boards
- *    summary: Get boards by member ID
- *    description:
- *      Will return the boards in which the member ID matches with the param user ID.
- *    produces: application/json
- *    parameters:
- *     - in: path
- *       name: ID
- *       schema:
- *         type: integer
- *         required: true
- *         description: The ID of the user to get
- *
- *    responses:
- *      200:
- *        description: Successful request
- *      5XX:
- *        description: Unexpected error.
- */
-router.get('/:memberId/boards/member', (req, res, next) => {
-  boardsController
-    .getBoardsByMemberId(req.params.memberId)
-    .then((result) => {
-      return res.json(result);
-    })
-    .catch(next);
-});
+router.use('/:memberId/boards/member', boardsRouter);
 
 module.exports = router;
