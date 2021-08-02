077cdc4a96af2e844ee1738ed2a8871124c64f18
karokea <karo7494@kea.stud.dk>
2021-08-02 16:18:52 +0300

    users controller and users router files with implemented user search endpoint

---
+++ src/server/api/controllers/users.controller.js
@@ -0,0 +1,26 @@
+const knex = require('../../config/db');
+const HttpError = require('../lib/utils/http-error');
+
+const getUsersByKeyword = async (searchWord) => {
+  if (!searchWord) {
+    throw new HttpError('Use valid keyword', 400);
+  }
+
+  try {
+    const users = await knex('users').where(
+      'fullname',
+      'like',
+      `%${searchWord}%`,
+    );
+    if (users.length === 0) {
+      throw new Error(`Cannot find user with name like '${searchWord}'`, 404);
+    }
+    return users;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+module.exports = {
+  getUsersByKeyword,
+};
---
+++ src/server/api/routes/users.router.js
@@ -0,0 +1,42 @@
+/* TODO: This is just an example file to illustrate API routing and
+documentation. Can be deleted when the first real route is added. */
+
+const express = require('express');
+
+const router = express.Router({ mergeParams: true });
+
+// controllers
+const usersController = require('../controllers/users.controller');
+
+/**
+ * @swagger
+ * /users/search?fullName=keyword:
+ *  get:
+ *    tags:
+ *    - Users
+ *    summary: Get users by search word
+ *    description:
+ *      Will return users whos full name match the search word.
+ *    produces: application/json
+ *    parameters:
+ *     - in: query
+ *       name: fullName
+ *       schema:
+ *         type: string
+ *         required: true
+ *         description: search word to match against the user's full name
+ *
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.get('/search', (req, res, next) => {
+  usersController
+    .getUsersByKeyword(req.query.fullName)
+    .then((result) => res.json(result))
+    .catch(next);
+});
+
+module.exports = router;
