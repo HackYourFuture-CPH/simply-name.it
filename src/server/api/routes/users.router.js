53e6b3ec905f4ec423b363ba7f066c46eec46208
nig <nielshtg@hotmail.com>
2021-08-03 17:09:36 +0200

    fix routing from users to boards

--- src/server/api/routes/users.router.js
+++ src/server/api/routes/users.router.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const boardsRouter = require('./boards.router');
 
-router.use('/:memberId/boards/member', boardsRouter);
+router.use('/:userId/boards', boardsRouter);
 
 module.exports = router;
