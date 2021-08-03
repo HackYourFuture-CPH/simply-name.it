114ea00f2a60a45f50049cd36077c2330749a3ea
Rasim-Deniz <rasimdeniz1996@gmail.com>
2021-08-03 16:03:46 +0200

    Handled some build errors

--- src/server/api/controllers/update-ballots.controller.js
+++ src/server/api/controllers/update-ballots.controller.js
@@ -1,11 +1,13 @@
-const { queries } = require('@testing-library/react');
 const knex = require('../../config/db');
 const HttpError = require('../lib/utils/http-error');
 
 const editBallots = async (userId, boardId, candidates) => {
+  if (!userId && !boardId) {
+    throw new HttpError('moduleId should be a number', 400);
+  }
   const trx = await knex.transaction();
   const queries = candidates.map((candidate) => {
-    knex('ballots')
+    return knex('ballots')
       .where({ userId, boardId, candidateId: candidate.candidateId })
       .update({ rank: candidate.rank })
       .transacting(trx);
