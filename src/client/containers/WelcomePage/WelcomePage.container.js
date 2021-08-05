cf22483b9d7deaf75df1eb807da705badf6409c2
karokea <karo7494@kea.stud.dk>
2021-08-05 23:22:24 +0200

    initial draft of Welcome page. Need to work on background size and positioning of elements

--- src/client/containers/WelcomePage/WelcomePage.container.js
+++ src/client/containers/WelcomePage/WelcomePage.container.js
@@ -1,5 +1,36 @@
 import React from 'react';
+import './WelcomePage.styles.css';
+
+import background from '../../assets/images/pages-background/Login.svg';
+import Button from '../../components/Button/Button.component';
+import GoogleButton from '../../components/GoogleButton/GoogleButton.component';
 
 export default function Welcome() {
-  return <div>This is your private Welcome</div>;
+  const onClick = () => {
+    console.log('you clicked!');
+  };
+  return (
+    <div className="sign-in-div">
+      {/* <img
+        src={background}
+        className="welcome-background"
+        alt="background blobs"
+      /> */}
+      <div className="welcome-slogan-container">
+        <h1 className="welcome-slogan">Take Tough Decisions Together</h1>
+      </div>
+      <div className="sign-in-buttons">
+        <GoogleButton className="google-button" />
+        <Button
+          className="generic-button"
+          buttonLabel="Login"
+          onClick={onClick}
+          size="large"
+          buttonColor="white"
+          buttonBorder="purple-gradient"
+          buttonLabelColor="purple"
+        />
+      </div>
+    </div>
+  );
 }
