dc919b4bb539956eee9ae194508e8e26ef94d552
Islam Fawzy <islam.fawzy25@hotmail.com>
2021-08-05 11:13:28 +0200

    need addressed positioning

--- src/client/components/HeaderComponent/HeaderComponent.component.js
+++ src/client/components/HeaderComponent/HeaderComponent.component.js
@@ -2,20 +2,27 @@ import React from 'react';
 import './HeaderComponent.styles.css';
 import PropTypes from 'prop-types';
 
-export default function HeaderComponent({ src, alt, children }) {
+export default function HeaderComponent({ variant, src, alt, children }) {
   return (
     <div className="headerComponent-container">
-      <img src={src} alt={alt} className="headerComponent-image" />
+      <img src={src} alt={alt} className={`headerComponent-${variant}`} />
+      <div className={`headerComponent-${variant}`}> </div>
       <div className="headerComponent-main">
-        <div className="headerComponent-title">{arrowButton}</div>
-        <div className="headerComponent-children">{dropDownButton}</div>
+        <div className="headerComponent-children">{children}</div>
       </div>
     </div>
   );
 }
 
 HeaderComponent.propTypes = {
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
+  src: PropTypes.string,
+  alt: PropTypes.string,
   children: PropTypes.node.isRequired,
+  variant: PropTypes.string,
+};
+
+HeaderComponent.defaultProps = {
+  src: 'https://i.ibb.co/nqysxTN/photo-1583710457367-47de0ea21fef.jpg',
+  alt: 'background-image',
+  variant: 'color',
 };
--- src/client/components/HeaderComponent/HeaderComponent.stories.js
+++ src/client/components/HeaderComponent/HeaderComponent.stories.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './HeaderComponent.styles.css';
 import HeaderComponent from './HeaderComponent.component.js';
-import { text, boolean } from '@storybook/addon-knobs';
+import { boolean, select } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import Dropdown from '../Dropdown/Dropdown.component';
+import ArrowButton from '../ArrowButton/ArrowButton.component';
 
 export default {
   title: 'Components / HeaderComponent',
 };
 
 export const HeaderComponentWithPurpleBackground = () => (
-  <HeaderComponent
-    src={text('Image', 'https://i.ibb.co/n84WtZW/maxresdefault.jpg')}
-    alt="purple-background"
-  >
+  <HeaderComponent variant={select('background-type', ['color', 'image'])}>
     <Dropdown
       variant="dark"
       visible={boolean('Menu Options', false)}
@@ -25,27 +23,6 @@ export const HeaderComponentWithPurpleBackground = () => (
         <li>Option 3</li>
       </ul>
     </Dropdown>
-  </HeaderComponent>
-);
-
-export const HeaderComponentWithoutPurpleBackground = () => (
-  <HeaderComponent
-    src={text(
-      'Image',
-      'https://i.ibb.co/nqysxTN/photo-1583710457367-47de0ea21fef.jpg',
-    )}
-    alt="baby-image"
-  >
-    <Dropdown
-      variant="dark"
-      visible={boolean('Menu Options', false)}
-      onClick={action('clicked')}
-    >
-      <ul>
-        <li>Option 1</li>
-        <li>Option 2</li>
-        <li>Option 3</li>
-      </ul>
-    </Dropdown>
+    <ArrowButton> </ArrowButton>
   </HeaderComponent>
 );
