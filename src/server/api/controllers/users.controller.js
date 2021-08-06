53d1b07f2ca88a3dbec72c40d21dbd2415227ad1
karokea <karo7494@kea.stud.dk>
2021-08-06 23:01:36 +0200

    Nat pranked me, we don't actually wanna do select '*'

--- src/server/api/controllers/users.controller.js
+++ src/server/api/controllers/users.controller.js
@@ -4,7 +4,13 @@ Can be deleted as soon as the first real controller is added. */
 const knex = require('../../config/db');
 
 const getUsers = () => {
-  return knex('users').select('*');
+  return knex('users').select(
+    'users.id',
+    'users.fullname',
+    'users.email',
+    'users.createdOn',
+    'users.firebaseUId',
+  );
 };
 
 module.exports = {
