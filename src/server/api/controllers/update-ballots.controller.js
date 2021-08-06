df245e8df9f3e9fde8ff7414b5d4c13bc8c0dcf8
Rasim-Deniz <rasimdeniz1996@gmail.com>
2021-08-06 15:33:22 +0200

    a logical operator is changed

--- src/server/api/controllers/update-ballots.controller.js
+++ src/server/api/controllers/update-ballots.controller.js
@@ -2,7 +2,7 @@ const knex = require('../../config/db');
 const { InvalidIdError } = require('../lib/utils/http-error');
 
 const editBallots = async (userId, boardId, candidates) => {
-  if (!Number.isInteger(Number(userId)) && !Number.isInteger(Number(boardId))) {
+  if (!Number.isInteger(Number(userId)) || !Number.isInteger(Number(boardId))) {
     throw new InvalidIdError('userId and boardId should be integers');
   }
 
