34b65e0c9b2a0cb7da11f81215c6bb795644db1b
karokea <karo7494@kea.stud.dk>
2021-08-08 23:35:04 +0200

    post bbq confusion 2

--- src/server/api/routes/users.router.js
+++ src/server/api/routes/users.router.js
@@ -1,13 +1,3 @@
-import React from 'react';
-import Welcome from './WelcomePage.container';
-
-export default {
-  title: 'containers / Welcome page ',
-  component: WelcomePage,
-};
-
-export const WelcomePage = () => <Welcome />;
-
 const express = require('express');
 
 const router = express.Router({ mergeParams: true });
