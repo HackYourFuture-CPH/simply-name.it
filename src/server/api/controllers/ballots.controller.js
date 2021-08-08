e2a13e4b84d5424b10f552a23ee0dac445f09d5c
Rasim-Deniz <rasimdeniz1996@gmail.com>
2021-08-08 15:45:17 +0200

    Validation for array is added and made some restoration

--- src/server/api/controllers/ballots.controller.js
+++ src/server/api/controllers/ballots.controller.js
@@ -18,13 +18,29 @@ const editBallots = async (userId, boardId, candidates) => {
   ) {
     throw new IncorrectEntryError(`candidateId and rank should be integers`);
   } else {
-    return knex.transaction(function (trx) {
-      const queries = candidates.map((candidate) => {
-        return trx('ballots')
-          .where({ userId, boardId, candidateId: candidate.candidateId })
-          .update({ rank: candidate.rank });
-      });
-      return Promise.all(queries);
+    return knex.transaction(async (trx) => {
+      const candidateIds = await trx('ballots')
+        .where({ userId, boardId })
+        .select('candidateId');
+      const candidateIdsInDbSet = new Set(
+        candidateIds.map((candidate) => candidate.candidateId),
+      );
+      const candidateIdsSet = new Set(
+        candidates.map((candidate) => candidate.candidateId),
+      );
+      const intersection = [...candidateIdsSet].filter((ids) =>
+        candidateIdsInDbSet.has(ids),
+      );
+      if (intersection.length !== candidates.length) {
+        throw new IncorrectEntryError(`candidateId is not found`);
+      } else {
+        const queries = candidates.map(async (candidate) => {
+          return await trx('ballots')
+            .where({ userId, boardId, candidateId: candidate.candidateId })
+            .update({ rank: candidate.rank });
+        });
+        return Promise.all(queries);
+      }
     });
   }
 };
