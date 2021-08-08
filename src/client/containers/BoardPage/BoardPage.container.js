bb4762101eff5370c8e61d8ed5dc61af8e84d60d
Hani-far <hfarjamy@outlook.com>
2021-08-08 16:00:47 +0200

    Board page created

--- src/client/containers/BoardPage/BoardPage.container.js
+++ src/client/containers/BoardPage/BoardPage.container.js
@@ -1,3 +1,62 @@
 import React from 'react';
+import './BoardPage.style.css';
+import GenericButton from '../../components/GenericButton/GenericButton.component';
+import BoardImg from '../../assets/images/demo-boards-photos/Board1.jpg';
+import PageTitle from '../../components/PageTitle/PageTitle.component';
+import Input from '../../components/InputComponent/InputComponent';
+import CardList from '../../components/CandidateCard/CandidateCardList.component';
+import { candidateListArr } from '../../components/CandidateCard/CandidateListArray';
 
-export const Board = () => <div className="Board ">The Board!</div>;
+export default function Board() {
+  const onClick = () => {
+    // console.log('you clicked!');
+  };
+  const onChange = () => {
+    // console.log('you clicked!');
+  };
+  const candidateList = candidateListArr();
+  return (
+    <div className="Board-container">
+      <div className="Header-component">
+        <img src={BoardImg} alt="BoardImg" width="100%" />
+      </div>
+      <div className="title">
+        <PageTitle title="Baby name" />
+        <GenericButton
+          className="generic-button"
+          buttonSize="small"
+          buttonType="secondary"
+          buttonDisabled={false}
+          onClick={onClick}
+          buttonLabel="Members"
+        />
+      </div>
+      <div className="Input-component">
+        <Input
+          type="text"
+          placeholder="Add candidate..."
+          theme="dark"
+          borderShape="curved"
+          onChange={onChange}
+        />
+      </div>
+      <div className="CandidateCard-component">
+        <CardList
+          variant="primary-color"
+          candidateList={candidateList}
+          display="visible"
+        />
+      </div>
+      <div className="Result">
+        <GenericButton
+          className="Result-button"
+          buttonLabel="Result"
+          buttonSize="medium"
+          buttonType="primary"
+          buttonDisabled={true}
+          onClick={onClick}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/client/containers/BoardPage/BoardPage.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Board from './BoardPage.container';
+
+export default {
+  title: 'containers / Board page ',
+  component: BoardPage,
+};
+
+export const BoardPage = () => <Board />;
