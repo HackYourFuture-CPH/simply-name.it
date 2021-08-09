0068abdbf80395e5c893400ff522e47a56c6cb64
karokea <karo7494@kea.stud.dk>
2021-08-09 02:01:43 +0200

    added 'welcome'path

--- src/client/App.js
+++ src/client/App.js
@@ -10,6 +10,7 @@ import { useAuthentication } from './hooks/useAuthentication';
 import Header from './components/Navigation/Header';
 import ProfilePage from './containers/ProfilePage/ProfilePage.container';
 import Loader from './components/Loader';
+import Welcome from './containers/WelcomePage/WelcomePage.container';
 
 function App() {
   const { isLoading } = useAuthentication();
@@ -31,6 +32,7 @@ function App() {
         <SignIn exact path="/sign-in" />
         <SignUp exact path="/sign-up" />
         <ResetPassword exact path="/reset-password" />
+        <Welcome exact path="/welcome" />
 
         {/* All routes below are authenticated routes - a user must login first */}
         <AuthenticatedRoute exact path="/profile">
