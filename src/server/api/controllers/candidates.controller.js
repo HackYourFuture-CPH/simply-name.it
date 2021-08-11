f9a6c714a0d5f9486bff8d3f88a6421fcb9218a8
darinkagm <dariinkagm@gmail.com>
2021-08-11 22:47:09 +0200

    Feedback implemented on Candidates controller

--- src/server/api/controllers/candidates.controller.js
+++ src/server/api/controllers/candidates.controller.js
@@ -43,8 +43,20 @@ const createCandidate = async (userId, boardId, newCandidate) => {
   return createNewCandidate;
 };
 
+const getCandidates = async (userId, boardId) => {
+  const candidates = await knex('candidates')
+    .join('boards', 'candidates.boardId', '=', 'boards.id')
+    .join('ballots', 'ballots.candidateId', '=', 'candidates.id')
+    .select('candidates.name', 'ballots.rank')
+    .where('candidates.boardId', boardId)
+    .where('ballots.userId', userId);
+
+  return candidates;
+};
+
 module.exports = {
   createCandidate,
   deleteCandidate,
   checkUserRole,
+  getCandidates,
 };
--- src/server/api/routes/candidates.router.js
+++ src/server/api/routes/candidates.router.js
@@ -103,4 +103,47 @@ router.post('/', async (req, res) => {
   return res.status(201).send();
 });
 
+router.get('/', async (req, res) => {
+  const boardCandidates = await candidatesController.getCandidates(
+    req.params.userId,
+    req.params.boardId,
+  );
+  return res.json(boardCandidates);
+});
+
+/**
+ * @swagger
+ * /users/{userId}/boards/{boardId}/candidates:
+ *  get:
+ *    tags:
+ *    - candidates
+ *    summary: Get all candidates of a specific board
+ *    description:
+ *      Get all candidates of a specific board
+ *    produces: application/json
+ *    parameters:
+ *     - in: path
+ *       name: userId
+ *       schema:
+ *         type: integer
+ *         required: true
+ *         description: ID of the specified user
+ *     - in: path
+ *       name: boardId
+ *       schema:
+ *         type: integer
+ *         required: true
+ *         description: ID of the specified board
+ *
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error
+ *      400:
+ *          Invalid ID
+ *      404:
+ *        description: Invalid entry error
+ */
+
 module.exports = router;
