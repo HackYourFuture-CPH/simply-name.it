5ad608753bf722fc729e8ab9e73da58746472b25
darinkagm <dariinkagm@gmail.com>
2021-08-11 22:02:51 +0200

    Revert "Solved conflict with Islam's files"
    This reverts commit 794edf46dfb78a010c1aecfdb41ddff1595700b5, reversing
    changes made to 79dddb1bdb25655def6fbe7ff9ef6cbd5372b17f.

--- src/server/api/controllers/candidates.controller.js
+++ src/server/api/controllers/candidates.controller.js
@@ -1,65 +1,16 @@
 const knex = require('../../config/db');
 
-const {
-  InvalidIdError,
-  IncorrectEntryError,
-  InvalidRequestError,
-} = require('../lib/utils/http-error');
-
-const checkUserRole = async ({ userId, boardId }) => {
-  const ifuserIsCreator = await knex('boards')
-    .where('boards.creatorId', userId)
-    .andWhere('boards.id', boardId);
-
-  if (!ifuserIsCreator) {
-    throw new IncorrectEntryError(`Only creator can take this action `);
-  }
-};
-
-const deleteCandidate = async ({ candidateId, userId, boardId }) => {
-  await checkUserRole({ userId, boardId });
-  await knex('candidates')
-    .where({ id: candidateId })
-    .update({ isBlocked: true });
-};
-
-const createCandidate = async (userId, boardId, newCandidate) => {
-  if (!Number.isInteger(Number(userId)) || !Number.isInteger(Number(boardId))) {
-    throw new InvalidIdError('Id should be an integer');
-  }
-  if (Object.keys(newCandidate).length === 0) {
-    throw new InvalidRequestError(
-      `key 'name' and value of type 'string' is required`,
-    );
-  }
-  if (typeof newCandidate.name !== 'string') {
-    throw new IncorrectEntryError(`Candidate name should be string`);
-  }
-  const createNewCandidate = await knex('candidates').insert({
-    boardId,
-    name: newCandidate.name,
-    isBlocked: false,
-  });
-  return createNewCandidate;
-};
-
 const getCandidates = async (userId, boardId) => {
-  if (!Number.isInteger(Number(userId)) || !Number.isInteger(Number(boardId))) {
-    throw new InvalidIdError('Id should be an integer');
-  }
   const candidates = await knex('candidates')
     .join('boards', 'candidates.boardId', '=', 'boards.id')
     .join('ballots', 'ballots.candidateId', '=', 'candidates.id')
     .select('candidates.name', 'ballots.rank')
     .where('candidates.boardId', boardId)
-    .where('ballots.userId', userId);
+    .where('ballots.userId', userId)
 
   return candidates;
 };
 
 module.exports = {
-  createCandidate,
-  deleteCandidate,
-  checkUserRole,
   getCandidates,
 };
