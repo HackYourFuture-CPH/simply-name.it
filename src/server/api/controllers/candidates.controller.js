79dddb1bdb25655def6fbe7ff9ef6cbd5372b17f
Darinka <75566763+DarinkaGM@users.noreply.github.com>
2021-08-11 19:11:49 +0200

    Update src/server/api/controllers/candidates.controller.js
    Co-authored-by: Rasim Deniz <75583909+Rasim-Deniz@users.noreply.github.com>

--- src/server/api/controllers/candidates.controller.js
+++ src/server/api/controllers/candidates.controller.js
@@ -2,13 +2,11 @@ const knex = require('../../config/db');
 
 const getCandidates = async (userId, boardId) => {
   const candidates = await knex('candidates')
-    .join('boards', 'candidates.boardId', '=', 'board.id')
-    .join('users', 'candidates.userId', '=', 'user.id')
-    .join('ballots', 'ballots.candidateId', '=', 'candidate.id')
-    .select('candidates.name', 'candidates.rank')
+    .join('boards', 'candidates.boardId', '=', 'boards.id')
+    .join('ballots', 'ballots.candidateId', '=', 'candidates.id')
+    .select('candidates.name', 'ballots.rank')
     .where('candidates.boardId', boardId)
-    .where('candidates.userId', userId)
-    .where('ballots.candidateId', candidateId);
+    .where('ballots.userId', userId)
 
   return candidates;
 };
