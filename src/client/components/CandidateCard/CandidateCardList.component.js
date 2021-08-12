ae763e4b06675281d8a8aaa94de56df759081ed5
Rasim-Deniz <rasimdeniz1996@gmail.com>
2021-08-12 13:13:24 +0200

    first implementation is done

--- src/client/components/CandidateCard/CandidateCardList.component.js
+++ src/client/components/CandidateCard/CandidateCardList.component.js
@@ -1,40 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './CandidateCardList.style.css';
 import DeleteIcon from './SvgIcon.component';
+import {
+  closestCenter,
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 const CardListItem = (props) => {
-  return (
-    <li>
-      <div className="card-container">
-        <div className="card-title">
-          <p>{props.candidate.name}</p>
-        </div>
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: props.candidate.id });
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
 
-        <div>
-          <span>&#8942;</span>
-          <span>&#8942;</span>
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <div className={`card-display ${props.variant}`}>
+        <div className={`delete-icon-display ${props.display}`}>
+          <DeleteIcon />
         </div>
+        <li>
+          <div className="card-container">
+            <div className="card-title">
+              <p>{props.candidate.name}</p>
+            </div>
+
+            <div>
+              <span>&#8942;</span>
+              <span>&#8942;</span>
+            </div>
+          </div>
+        </li>
       </div>
-    </li>
+    </div>
   );
 };
 
 const CardList = ({ variant, candidateList, display }) => {
-  return (
-    <ul>
-      {candidateList.map((candidate) => {
-        return (
-          <div className={`card-display ${variant}`}>
-            <div className={`delete-icon-display ${display}`}>
-              <DeleteIcon />
-            </div>
-            <CardListItem candidate={candidate} key={candidate.id} />
-          </div>
-        );
-      })}
-    </ul>
+  const [items, setItems] = useState(candidateList);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
   );
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext items={items} strategy={verticalListSortingStrategy}>
+        <ul>
+          {items.map((candidate) => (
+            <CardListItem
+              key={candidate.id}
+              id={candidate.id}
+              candidate={candidate}
+              variant={variant}
+              display={display}
+            />
+          ))}
+        </ul>
+      </SortableContext>
+    </DndContext>
+  );
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
 };
 CardList.propTypes = {
   display: PropTypes.string.isRequired,
