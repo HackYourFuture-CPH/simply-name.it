23b5c4924c2ba7e123b36ff553e07fece7159866
Wenceslao Posse Silva <wencho22@gmail.com>
2021-08-13 13:56:45 +0200

    added onclickhandler

--- src/client/components/GoogleButton/GoogleButton.component.js
+++ src/client/components/GoogleButton/GoogleButton.component.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './GoogleButton.css';
 
-const GoogleButton = () => {
+const GoogleButton = (onClickHandler) => {
   return (
-    <div id="g-button">
+    <div
+      id="g-button"
+      onClick={() => onClickHandler}
+      role="button"
+      tabIndex={0}
+      onKeyUp={onClickHandler}
+    >
       <span className="google-icon" />
       <span className="buttonText">Sign up with Google</span>
     </div>
