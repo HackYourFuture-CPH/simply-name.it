1cca715188763d9c653f67e63805535c48845e1a
natsgt <nataliarodas@gmail.com>
2021-08-17 19:55:14 +0200

    erased some fields from select and added isDeleted conditional

--- src/server/api/controllers/boards.controller.js
+++ src/server/api/controllers/boards.controller.js
@@ -59,16 +59,11 @@ const getBoardsByCreatorId = async (id) => {
 
   const boards = await knex('users')
     .join('boards', 'users.id', '=', 'boards.creatorId')
-    .select(
-      'users.fullName',
-      'boards.id',
-      'boards.creatorId',
-      'boards.createdOn',
-      'boards.deadline',
-      'boards.isDeleted',
-      'boards.banner',
-    )
-    .where('creatorId', id);
+    .select('boards.id', 'boards.createdOn', 'boards.deadline', 'boards.banner')
+    .where({
+      creatorId: id,
+      'boards.isDeleted': false,
+    });
   if (boards.length === 0) {
     throw new IncorrectEntryError(`incorrect entry with the id of ${id}`);
   }
