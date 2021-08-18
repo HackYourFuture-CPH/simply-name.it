822a9cf17ad68a503a24a646f0adbf9e541ed62d
karokea <karo7494@kea.stud.dk>
2021-08-18 19:07:42 +0200

    moving, fetchin and seach works

--- src/server/api/controllers/users.controller.js
+++ src/server/api/controllers/users.controller.js
@@ -21,39 +21,71 @@ const { isInteger } = require('../lib/utils/validators');
 
 const createIndex = () => {};
 
-const createStudent = (document) => {
-  // TODO: Create student in DB
-  // const dbResult = await knex('students').insert(document, 'id')
+const createUser = async (document) => {
+  // Create user in DB
 
-  // Create student in ES
+  if (Object.keys(newUser).length === 0) {
+    throw new InvalidRequestError(
+      `key 'fullName, email, firebaseUId and value of type 'string' is required`,
+    );
+  }
+  if (typeof newUser.fullName !== 'string') {
+    throw new IncorrectEntryError(`fullName should be string`);
+  }
+  if (typeof newUser.email !== 'string') {
+    throw new IncorrectEntryError(`email should be string`);
+  }
+  if (typeof newUser.firebaseUId !== 'string') {
+    throw new IncorrectEntryError(`firebaseUId should be string`);
+  }
+  const createNewUser = await knex('users').insert({
+    fullName: newUser.fullName || 'ANONYMOUS',
+    email: newUser.email,
+    firebaseUId: newUser.firebaseUId || 'anonymous',
+  });
 
-  client.index({
+  // Create user in ES
+
+  await client.index({
     // id: dbResult.id, // TODO: set this to the ID from the DB
     index: index,
     body: document,
   });
 };
 
-const getUsers = async () => {
-  let i = Math.floor(Math.random() * 99);
-  const studentDocument = { name: `hehe${i}` };
-  createStudent(studentDocument);
+const moveUSersfromDBtoES = async () => {
+  const DBusers = await knex('users').select(
+    'users.id',
+    'users.fullname',
+    'users.email',
+    'users.createdOn',
+    'users.firebaseUId',
+  );
 
+  DBusers.map((user) => {
+    const DBuserDocument = {
+      name: user.fullname,
+      email: user.email,
+    };
+    createUser(DBuserDocument);
+  });
+};
+
+//moveUSersfromDBtoES();
+
+const getUsers = async () => {
   const result = await client.search({
     index: index,
     body: {
       query: {
         match_all: {},
       },
-      size: 20,
+      size: 50,
     },
   });
 
-  return result.body.hits.hits.map((hit) => ({ name: hit._source.name }));
+  return result.body.hits.hits.map((hit) => hit._source);
 
-  // return res.json(
-  //   result.body.hits.hits.map((hit) => ({ name: hit._source.name })),
-  // );
   // return knex('users').select(
   //   'users.id',
   //   'users.fullname',
@@ -83,13 +115,22 @@ const getUsersByKeyword = async (searchWord) => {
     throw new IncorrectEntryError('Use a keyword!', 400);
   }
 
-  const users = await knex('users').where(
-    'fullname',
-    'like',
-    `%${searchWord}%`,
-  );
+  // const users = await knex('users').where(
+  //   'fullname',
+  //   'like',
+  //   `%${searchWord}%`,
+  // );
+  const users = await client.search({
+    index: index,
+    body: {
+      query: {
+        match: { name: searchWord },
+      },
+      size: 20,
+    },
+  });
 
-  return users;
+  return users.body.hits.hits.map((hit) => hit._source);
 };
 
 module.exports = {
