4c14f1347122a0237d66dab364c598d0fc9e3df5
Hani-far <hfarjamy@outlook.com>
2021-08-20 14:27:08 +0200

    feedback implemented

--- src/client/containers/BoardPageComponents/BoardAddCandidate/AddCandidate.js
+++ src/client/containers/BoardPageComponents/BoardAddCandidate/AddCandidate.js
@@ -1,41 +1,20 @@
 // import React, { useState } from 'react';
 import postData from './PostData';
 
-export default async function AddCandidate(
-  newCandidate,
-  userId,
-  boardId,
-  setCandidates,
-  candidates,
-  setAddCandidateError,
-  setAddCandidateSuccess,
-) {
+export default async function AddCandidate(newCandidate, userId, boardId) {
   try {
     if (!newCandidate.name) {
-      setAddCandidateError('Please put a candidate name');
+      console.log('Please put a candidate name');
       return;
     }
 
     if (!isNaN(newCandidate.name)) {
-      setAddCandidateError('Candidate name can not be number');
+      console.log('Candidate name can not be number');
       return;
     }
 
     postData(`/api/users/${userId}/boards/${boardId}/candidates`, {
       name: newCandidate.name,
-    });
-    setAddCandidateSuccess(true);
-    setAddCandidateError(null);
-
-    setCandidates((prev) => {
-      const addNewCandidate = {
-        id:
-          candidates.length > 0 ? candidates[candidates.length - 1].id + 1 : 1,
-        boardId,
-        name: newCandidate.name,
-      };
-
-      return [...prev, addNewCandidate];
     });
   } catch (error) {
     throw new Error(error);
--- src/client/containers/BoardPageComponents/BoardAddCandidate/BoardAddCandidate.js
+++ src/client/containers/BoardPageComponents/BoardAddCandidate/BoardAddCandidate.js
@@ -3,14 +3,9 @@ import './BoardAddCandidate.style.css';
 import Input from '../../../components/InputComponent/InputComponent';
 import AddButton from '../../../components/AddButton/AddButton.component';
 import AddCandidate from './AddCandidate';
-import { candidateListArr } from '../../../components/CandidateCard/CandidateListArray';
 
 export default function BoardAddCandidate() {
   const [newCandidateName, setNewCandidateName] = useState('');
-  const [addCandidateError, setAddCandidateError] = useState(null);
-  const [addCandidateSuccess, setAddCandidateSuccess] = useState(false);
-  const [saveState, setSaveState] = useState(false);
-  const [candidates, setCandidates] = useState(candidateListArr());
 
   const userId = 2;
   const newCandidate = {
@@ -20,22 +15,9 @@ export default function BoardAddCandidate() {
   };
 
   const saveCandidate = (e) => {
-    setSaveState(true);
     e.preventDefault();
-    AddCandidate(
-      newCandidate,
-      userId,
-      newCandidate.boardId,
-      setCandidates,
-      candidates,
-      setAddCandidateError,
-      setAddCandidateSuccess,
-    );
+    AddCandidate(newCandidate, userId, newCandidate.boardId);
     setNewCandidateName('');
-    const timerId = setTimeout(async () => {
-      setSaveState(false);
-    }, 2000);
-    return () => clearTimeout(timerId);
   };
 
   return (
@@ -56,18 +38,6 @@ export default function BoardAddCandidate() {
           onClick={saveCandidate}
         />
       </div>
-      {saveState && (
-        <div>
-          {addCandidateError !== null && (
-            <div className="errorMessageContainer">{addCandidateError}</div>
-          )}
-          {addCandidateError === null && addCandidateSuccess && (
-            <div className="successMessageContainer">
-              The new candidate was stored successfully!
-            </div>
-          )}
-        </div>
-      )}
     </div>
   );
 }
