7f6a7c0c43760057be27122b8f3aab36a3e73d1b
darinkagm <dariinkagm@gmail.com>
2021-08-22 21:45:14 +0200

    Upload preview implemented(to be fixed)

--- src/client/components/Dropzone/dropzone.component.js
+++ src/client/components/Dropzone/dropzone.component.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './dropzone.styles.css';
 import { useDropzone } from 'react-dropzone';
 import AddImageIcon from './AddImageIcon.component';
 
+const thumbsContainer = {};
+
+const thumb = {
+  width: 325,
+  height: 200,
+};
+
+const thumbInner = {
+  overflow: 'hidden',
+};
+
+const img = {
+  width: 'auto',
+  height: '100%',
+};
+
 export default function DropzoneComp() {
-  const onDrop = (acceptedFiles) => acceptedFiles.map((file) => file.name);
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+  const [files, setFiles] = useState([]);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/*',
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          }),
+        ),
+      );
+    },
+  });
+  const thumbs = files.map((file) => (
+    <div style={thumb} key={file.name}>
+      <div style={thumbInner}>
+        <img src={file.preview} style={img} alt="imagepreview" />
+      </div>
+    </div>
+  ));
+  useEffect(
+    () => () => {
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    },
+    [files],
+  );
+
   return (
     <div className="DropzoneComp">
       <useDropzone>
@@ -14,6 +56,7 @@ export default function DropzoneComp() {
           <p>
             <AddImageIcon /> Browse
           </p>
+          <aside style={thumbsContainer}>{thumbs}</aside>
         </div>
       </useDropzone>
     </div>
