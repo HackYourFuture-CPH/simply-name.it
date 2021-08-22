0d9710496db6968d440ac6839f10669e7afc685d
Saidemm <saymahtab@gmail.com>
2021-08-22 09:40:36 +0200

    fixed the useEffect loop problem

--- src/client/containers/BoardPage/BoardPage.container.js
+++ src/client/containers/BoardPage/BoardPage.container.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import './BoardPage.style.css';
 import OwnerBoardPage from './OwnerBoardPage';
 import MemberBoardPage from './MemberBoardPage';
-import { useUser } from '../../firebase/UserContext';
+// import { useUser } from '../../firebase/UserContext';
 import { useBoard } from './BoardProvider';
 
 export default function BoardPage() {
-  const boardContext = useBoard();
+  const { isBoardLoading } = useBoard();
+  const { setBoardLoading } = useBoard();
+  // const {user} = useUser();
+  // console.log('user', user[0].id);
   const [boardInfo, setBoardInfo] = useState([]);
-  const userInfo = useUser();
 
   // const { userId, boardId } = useParams();
   const userId = 2;
@@ -21,21 +23,26 @@ export default function BoardPage() {
         const apiResponse = await fetch(API_URL);
         const apiData = await apiResponse.json();
         setBoardInfo(apiData[0]);
-        boardContext.setLoading(false);
+        setBoardLoading(false);
       } catch (error) {
         throw new Error(error);
       }
     };
-    fetchingBoardApi();
-  }, [boardContext]);
+    if (isBoardLoading) {
+      fetchingBoardApi();
+    }
+  }, [isBoardLoading, setBoardLoading]);
 
-  if (boardContext.Loading) {
+  if (isBoardLoading) {
     return <div>LOADING....</div>;
   }
 
   return userId === boardInfo.creatorId ? (
-    <OwnerBoardPage boardInfo={boardInfo} userInfo={userInfo} />
+    <>
+      {/* <button type="button" onClick={() => setBoardLoading(true)} style={{position:"absolute",top:"0px",right:"0px"}}>test</button> */}
+      <OwnerBoardPage boardInfo={boardInfo} />
+    </>
   ) : (
-    <MemberBoardPage boardInfo={boardInfo} userInfo={userInfo} />
+    <MemberBoardPage boardInfo={boardInfo} />
   );
 }
--- src/client/containers/BoardPage/BoardProvider.js
+++ src/client/containers/BoardPage/BoardProvider.js
@@ -4,15 +4,18 @@ import './BoardPage.style.css';
 const BoardContext = createContext();
 
 export default function BoardProvider({ children }) {
-  const [Loading, setLoading] = useState(true);
-  const [CandidateListLoading, setCandidateListLoading] = useState(true);
+  const [isBoardLoading, setBoardLoading] = useState(true);
+  const [isAddingCandidate, setAddingCandidate] = useState(true);
+  const [isCandidateListLoading, setCandidateListLoading] = useState(true);
 
   return (
     <BoardContext.Provider
       value={{
-        Loading,
-        setLoading,
-        CandidateListLoading,
+        isBoardLoading,
+        setBoardLoading,
+        isAddingCandidate,
+        setAddingCandidate,
+        isCandidateListLoading,
         setCandidateListLoading,
       }}
     >
