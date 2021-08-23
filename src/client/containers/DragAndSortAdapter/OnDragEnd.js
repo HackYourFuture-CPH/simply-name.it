6e2bc8379c0f16d6b88edd1a3f70c829c3634a8e
Rasim-Deniz <rasimdeniz1996@gmail.com>
2021-08-23 11:47:15 +0200

    updateBallots modified

--- src/client/containers/DragAndSortAdapter/OnDragEnd.js
+++ src/client/containers/DragAndSortAdapter/OnDragEnd.js
@@ -1,9 +1,13 @@
 import { arrayMove } from '@dnd-kit/sortable';
 
-export function onDragEnd(setItems, sortFunction, transformFunction) {
+export function onDragEnd(
+  setItems,
+  sortFunction,
+  transformFunction,
+  setDraggedInit,
+) {
   return function (event) {
     const { active, over } = event;
-
     if (active.id !== over.id) {
       setItems((draggedItems) => {
         const { oldIndex, newIndex } = sortFunction(
@@ -11,7 +15,7 @@ export function onDragEnd(setItems, sortFunction, transformFunction) {
           active.id,
           over.id,
         );
-
+        setDraggedInit(true);
         return arrayMove(draggedItems, oldIndex, newIndex).map(
           transformFunction,
         );
