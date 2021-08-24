3832938d3a3e29bf8c1c177baf0633971fd15afb
Gohar33 <vangogik@gmail.com>
2021-08-24 13:03:08 +0200

    BoardHeader seperate container for BoardPage

---
+++ src/client/containers/BoardPageComponents/BoardAddCandidate/BoardHeader.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './BoardHeader.style.css';
+import HeaderComponent from '../../../components/HeaderComponent/Header.component.js';
+import Dropdown from '../../../components/Dropdown/Dropdown.component';
+import ArrowButton from '../../../components/ArrowButton/ArrowButton.component';
+import { Link } from 'react-router-dom';
+
+export default function BoardHeader() {
+  const [visibility, setVisibility] = useState(false);
+  const closeDropdown = () => {
+    if (visibility === false) {
+      setVisibility(true);
+    } else {
+      setVisibility(false);
+    }
+  };
+  const onClick = () => {
+    // console.log('you clicked!');
+  };
+  return (
+    <div className="Board-container">
+      <HeaderComponent>
+        <Link to="/profile">
+          <ArrowButton className="arrow-button-white" onClick={onClick} />
+        </Link>
+        <Dropdown
+          className="dropdown-button"
+          variant="dark"
+          visible={visibility}
+          onClick={closeDropdown}
+        >
+          <div className="options-container dark">
+            <ul className="option-list">
+              <li>Edit Board</li>
+              <li>Delete Board</li>
+            </ul>
+          </div>
+        </Dropdown>
+      </HeaderComponent>
+    </div>
+  );
+}
