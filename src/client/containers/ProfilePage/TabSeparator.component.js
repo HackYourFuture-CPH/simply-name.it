99b5462f5ece0c7751d81fd6c93a512e103d74a2
natsgt <nataliarodas@gmail.com>
2021-08-28 14:40:48 +0200

    tab separator component

---
+++ src/client/containers/ProfilePage/TabSeparator.component.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import './TabSeparator.styles.css';
+import ProfileProps from './ProfileContext';
+
+export default function TapSeparator() {
+  const { setOnMyBoards } = useContext(ProfileProps);
+
+  return (
+    <>
+      <input
+        type="radio"
+        name="boards"
+        id="tab-1"
+        className="tab-1"
+        defaultChecked
+        onClick={() => {
+          setOnMyBoards(true);
+        }}
+      />
+      <span>My Boards</span>
+      <input
+        type="radio"
+        name="boards"
+        id="tab-2"
+        className="tab-2"
+        onClick={() => {
+          setOnMyBoards(false);
+        }}
+      />
+      <span>Joined Boards</span>
+    </>
+  );
+}
