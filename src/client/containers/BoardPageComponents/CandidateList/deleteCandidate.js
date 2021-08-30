066ec8a30ad569ab52dcee82268bd0f802e41c91
Rasim-Deniz <rasimdeniz1996@gmail.com>
2021-08-30 12:52:52 +0200

    error handler is added

--- src/client/containers/BoardPageComponents/CandidateList/deleteCandidate.js
+++ src/client/containers/BoardPageComponents/CandidateList/deleteCandidate.js
@@ -9,5 +9,9 @@ export async function deleteCandidate(userId, boardId, candidateId) {
       },
     },
   );
-  return response;
+  if (!response.ok) {
+    throw new Error(response.statusText, response.status);
+  } else {
+    return response;
+  }
 }
