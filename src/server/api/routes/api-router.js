e8c873cee115ca1ecff54a619160bd4d06b4a4ed
natsgt <nataliarodas@gmail.com>
2021-08-31 17:08:46 +0200

    fixed location of file

--- src/server/api/routes/api-router.js
+++ src/server/api/routes/api-router.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { authenticate } = require('./middleware/auth');
+const { authenticate } = require('../../middleware/auth');
 
 // Router imports
 
